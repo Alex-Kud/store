d8c0b57eb01b2ee9b9a2c2002789fabfbd40566f
alexander.kudashov <sasha.kudaschov2014@yandex.ru>
2023-07-06 16:18:37 +0300

    The connection to the database and the Liquibase migration system are configured. The first database tables have been created and filled in.

--- store/src/main/java/com/practice/store/StoreApplication.java
+++ store/src/main/java/com/practice/store/StoreApplication.java
@@ -1,12 +1,12 @@
 package com.practice.store;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @SpringBootApplication
 public class StoreApplication {
 
@@ -14,14 +14,15 @@ public class StoreApplication {
 		SpringApplication.run(StoreApplication.class, args);
 		System.out.println("This is test console log");
 	}
-	
+
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
 
-	@GetMapping("/sample")
-	public String showForm() {
-		return "sample";
+	@GetMapping("/test")
+	public String test() {
+		return "test";
 	}
+
 }
