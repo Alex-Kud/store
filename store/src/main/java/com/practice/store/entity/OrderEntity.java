0147fbbdc22b4f7ccb2a0a62b4c2765149d1fb92
alexander.kudashov <sasha.kudaschov2014@yandex.ru>
2023-07-07 19:00:47 +0300

    Added the "order" entity class

---
+++ store/src/main/java/com/practice/store/entity/OrderEntity.java
@@ -0,0 +1,69 @@
+package com.practice.store.entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "orders")
+public class OrderEntity implements Serializable {
+    @Id
+    @Column(name = "id", nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "buyer")
+    private BuyerEntity buyer;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "product")
+    private ProductEntity product;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+    @Column(name = "date", nullable = false)
+    private Date date;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public BuyerEntity getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(BuyerEntity buyer) {
+        this.buyer = buyer;
+    }
+
+    public ProductEntity getProduct() {
+        return product;
+    }
+
+    public void setProduct(ProductEntity product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
